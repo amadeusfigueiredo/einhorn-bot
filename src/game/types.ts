51aa11f63050eb8cd0a9dc05eb993ac23673bb64
amafigu <amadeusfigue@gmail.com>
2025-10-30 11:48:41 +0100

    organize types

--- src/game/types.ts
+++ src/game/types.ts
@@ -1,5 +1,12 @@
 export type Keys = Record<string, boolean>
-export type SceneName = string
+export type SceneName =
+  | 'stage1'
+  | 'stage2'
+  | 'stage3'
+  | 'stage4'
+  | 'stage5'
+  | 'stage6'
+  | 'stage7'
 
 export type Rect = { x: number; y: number; w: number; h: number }
 
@@ -23,18 +30,57 @@ export type NPC = {
   r?: number // draw radius (default 26)
   talkRadius?: number // distance to allow talking (default 80)
   question: Question
+  imageKey?: string
 }
 
 export type StageConfig = {
   name: SceneName
-  bg?: 'park' | 'street' | 'school' | 'custom'
+  background?:
+    | 'stage1Background'
+    | 'stage2Background'
+    | 'stage3Background'
+    | 'stage4Background'
+    | 'stage5Background'
+    | 'stage6Background'
+    | 'stage7Background'
+
   requiredToAdvance?: number // default: all gates + all NPCs
   nextStage?: SceneName | null
   gates?: Gate[] // optional now
   npcs?: NPC[] // NEW
 }
 
-export type Assets = {
-  unicorn?: HTMLImageElement
-  backgrounds?: Record<string, HTMLImageElement>
+export type Npc = {
+  id: string
+  x: number
+  y: number
+  question: {
+    id: string
+    prompt: string
+    choices: string[]
+    correctIndex: number
+  }
+}
+
+export type LoaderImageAssets = {
+  stage1Background?: HTMLImageElement
+  stage2Background?: HTMLImageElement
+  stage3Background?: HTMLImageElement
+  stage4Background?: HTMLImageElement
+  stage5Background?: HTMLImageElement
+  stage6Background?: HTMLImageElement
+  stage7Background?: HTMLImageElement
+  stage8Background?: HTMLImageElement
+  stage1Npc1?: HTMLImageElement
+  stage1Npc2?: HTMLImageElement
+  stage1Npc3?: HTMLImageElement
+  stage1Npc4?: HTMLImageElement
+  stage2Npc1?: HTMLImageElement
+  player1?: HTMLImageElement
+}
+
+export type LoaderAudioAssets = {
+  stage1?: HTMLAudioElement
+  stage2?: HTMLAudioElement
+  stage3?: HTMLAudioElement
 }
